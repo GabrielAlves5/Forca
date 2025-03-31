a9f87f7c61d28e3136b58bba3f86117dd3dc20d3
GabrielAlves5 <ecgabriel.alves@gmail.com>
2025-03-31 11:30:06 -0300

    Forca

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string letra, palavrasecreta;
+Console.WriteLine ("Qual a palavra:");
+palavrasecreta = Console.ReadLine().ToUpper();
+Console.WriteLine ("Fala uma letra");
+letra = Console.ReadLine().ToUpper();
+Console.WriteLine($"Essa letra {letra} esta em {palavrasecreta}? \n{palavrasecreta.Contains(letra)}");
